223b4be40be5110d0b10d89455610aa891cd18d2
zulfiquarstorage1-del <zulfiquarstorage1@gmail.com>
2026-07-13 10:03:56 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = "https://vhkxrhuygujrghgjikdu.supabase.co";
 const SUPABASE_ANON_KEY = "sb_publishable_NaZ5L8e0wq_hzQ8WNCk_cQ_zMPI5FB-";
 
-
+const authForm = document.getElementById('login-form');
 // Add the 'async' keyword right before the (e) => arrow function
 authForm.addEventListener('submit', async (e) => {
     e.preventDefault();
