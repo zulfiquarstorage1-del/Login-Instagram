d54be972180a27ec04e9268d947ae21821b0eda7
zulfiquarstorage1-del <zulfiquarstorage1@gmail.com>
2026-07-13 09:34:30 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+const SUPABASE_URL = "https://vhkxrhuygujrghgjikdu.supabase.co";
+const SUPABASE_ANON_KEY = "sb_publishable_NaZ5L8e0wq_hzQ8WNCk_cQ_zMPI5FB-";
+
+
+
 supabase.auth.signInWithPassword({ email, password })
